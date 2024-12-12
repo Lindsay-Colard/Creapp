1964c0732ab26bfce79ee13826c32f8a3fd21e52
Bastien Gathoye <bastiengathoye@gmail.com>
2024-12-12 11:12:17 +0100

    worked on cookie clicker

--- app.js
+++ app.js
@@ -2,18 +2,67 @@
 
 let today = new Date().toLocaleDateString();
 let previousCookieOpened = localStorage.getItem("cookieDate");
-if (today == previousCookieOpened) {
-    alert("You have already done your cookie today");
-}
 
 let cookieClicker = document.querySelector(".cookie");
 cookieClicker.addEventListener("click", cookieClicked);
 
-function cookieClicked(){
-    previousCookieOpened = localStorage.getItem("cookieDate");
+let messageTab;
+let messageDisplay;
+let messageBalise = document.querySelector(".message");
+let indicationBalise = document.querySelector(".indication");
+let cookieClickLeft = 3;
 
-    if(today != previousCookieOpened){
+let indicationChange = [
+    "Ooooh...",
+    "Tu y es presque !",
+    "Ton cookie est ouvert"
+];
+
+if (today == previousCookieOpened) {
+    localStorage.setItem("cookieDate", today);
+    messageDisplay = localStorage.getItem("messageSaved");
+    writeMessage();
+    lockBtn();
+    writeIndication(3);
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+fetch("assets/json/messages.json")
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        messageTab = structuredClone(json);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+function cookieClicked(){
+    if (cookieClickLeft == 0) {
         localStorage.setItem("cookieDate", today);
-        alert("Nice, you have open your cookie")
+        messageDisplay = messageTab[getRandomIntInclusive(0, messageTab.length - 1)];
+        localStorage.setItem("messageSaved", messageDisplay);
+        writeMessage();
+        lockBtn();
     }
+    cookieClickLeft--;
+    writeIndication(cookieClickLeft);
+}
+
+function lockBtn(){
+    cookieClicker.classList.toggle("blocked");
+}
+
+function writeMessage(){
+    messageBalise.innerText = messageDisplay;
+}
+
+function writeIndication(state){
+    indicationBalise.innerText = indicationChange[(indicationChange.length - 1) - state];
 }
